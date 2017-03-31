e135ecd37b2032abdcc75105b4ddfd4883e4e11d
pshen <patrick.lc.shen@gmail.com>
2017-03-31 15:54:43 -0700

    solution including negative nums

--- appleStock.js
+++ appleStock.js
@@ -1,8 +1,11 @@
 let max_profit = (stock_prices_yesterday) => {
+  if (stock_prices_yesterday.length < 2) {
+    return "One 1 price from yesterday. Unable to make profit."
+  }
   var min = stock_prices_yesterday[0];
-  var max = 0;
+  var max = stock_prices_yesterday[1] - stock_prices_yesterday[0];
   var profit;
-  for (var i = 0; i < stock_prices_yesterday[i].length; i++) {
+  for (var i = 1; i < stock_prices_yesterday[i].length; i++) {
     min = Math.min(min, stock_prices_yesterday[i]);
     profit = stock_prices_yesterday[i] - min;
     max = Math.max(max, profit);

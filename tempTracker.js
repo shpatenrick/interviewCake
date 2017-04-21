619ae9dbd0c587c4b4411646342dd415e802df3d
pshen <patrick.lc.shen@gmail.com>
2017-04-21 13:00:08 -0700

    update to O(1) time

--- tempTracker.js
+++ tempTracker.js
@@ -1,5 +1,4 @@
 function TempTracker = () => {
-  this.temps = [];
   this.len = 0;
   this.max = null;
   this.min = null;
@@ -9,25 +8,37 @@ function TempTracker = () => {
 
 TempTracker.prototype.insert = (temp) => {
   // records new temp
-  if (temp <= min) {
-    this.temps.unshift(temp);
-    this.min = temp;
-  }
-  else if (temp >= max) {
-    this.temps.push(temp);
-    this.max = temp;
-  }
-  else {
-    while (i < this.temps.length) {
-      if (temp <= this.temps[i + 1] && temp >= this.temps[i]) {
-        this.temps.splice(i, 0, temp);
-        i = Infinity;
 
+  if (this.len > 0) {
+    if (temp <= min) {
+      this.temps.unshift(temp);
+      this.min = temp;
+    }
+    else if (temp >= max) {
+      this.temps.push(temp);
+      this.max = temp;
+    }
+    else {
+      while (i < this.temps.length) {
+        if (temp <= this.temps[i + 1] && temp >= this.temps[i]) {
+          this.temps.splice(i, 0, temp);
+          i = Infinity;
+
+        }
       }
     }
+    this.len++;
+    // update mean;
+    this.mean = ((this.mean * (this.len - 1)) + temp) / (this.len);
+    // update mode;
+
+  } else {
+    this.len = 1;
+    this.max = temp;
+    this.min = temp;
+    this.mean = temp;
+    this.mode = temp;
   }
-  // update mean;
-  this.mean = ((this.mean * (this.temps.length - 1)) + temp) / (this.temps.length);
 };
 TempTracker.prototype.getMax = () => {
   // return highest temp so far
